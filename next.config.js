cc6db302df8e1f747241a96a90e1bf693fe7445c
Kris Cruz <kriscruz@kriss-mbp.lan>
2023-05-01 11:07:41 +0800

    next config

--- next.config.js
+++ next.config.js
@@ -11,8 +11,20 @@ module.exports = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "x-vercel-cache:", value: null },
-          { key: "x-vercel-id", value: null },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://starwars-characters-steel.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
       },
     ];
