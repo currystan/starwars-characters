c4e6e63230b4bf5defde47612e880a452df88242
Kris Cruz <kriscruz@kriss-mbp.lan>
2023-05-01 10:20:14 +0800

    Next config

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://starwars-characters-steel.vercel.app/",
+            value: "*",
           },
           {
             key: "Access-Control-Allow-Methods",
